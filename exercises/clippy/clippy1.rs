ee49a9b96133125160ade326b8461cd33b4344e1
sheldonhh <xphuangsg@gmail.com>
2024-04-22 19:37:38 +0800

    clippy1

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -1,7 +1,7 @@
 use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
+    let pi = f32::consts::PI; // 使用标准库中的 PI 常量
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
@@ -9,5 +9,5 @@ fn main() {
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
         radius, area
-    )
+    );
 }
