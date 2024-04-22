722134f8cf1986f89adeabd13cf2b12ef1ab6442
sheldonhh <xphuangsg@gmail.com>
2024-04-22 20:03:36 +0800

    add algorithm10

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,68 +1,51 @@
-/*
-	graph
-	This problem requires you to implement a basic graph functio
-*/
-// I AM NOT DONE
-
 use std::collections::{HashMap, HashSet};
 use std::fmt;
+
 #[derive(Debug, Clone)]
 pub struct NodeNotInGraph;
+
 impl fmt::Display for NodeNotInGraph {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "accessing a node that is not in the graph")
     }
 }
+
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
+
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
         UndirectedGraph {
             adjacency_table: HashMap::new(),
         }
     }
+
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
         &mut self.adjacency_table
     }
+
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
+
+    fn add_node(&mut self, node: &str) -> bool {
+        self.adjacency_table.entry(node.to_string()).or_insert(Vec::new()).is_empty()
+    }
+
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (node1, node2, weight) = edge;
+        self.adjacency_table.entry(node1.to_string()).or_default().push((node2.to_string(), weight));
+        self.adjacency_table.entry(node2.to_string()).or_default().push((node1.to_string(), weight));
     }
 }
+
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
-    // Add a node to the graph
-    fn add_node(&mut self, node: &str) -> bool {
-        // 检查节点是否已存在
-        if self.adjacency_table().contains_key(node) {
-            false // 节点已存在，不添加
-        } else {
-            // 节点不存在，添加并返回 true
-            self.adjacency_table_mutable().insert(node.to_string(), Vec::new());
-            true
-        }
-    }
-    // Add an edge to the graph
-    fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        // 解构边的元素
-        let (from, to, weight) = edge;
-        // 添加从 from 到 to 的边
-        self.adjacency_table_mutable()
-            .entry(from.to_string())
-            .or_insert_with(Vec::new)
-            .push((to.to_string(), weight));
-        // 由于是无向图，还需要添加从 to 到 from 的边
-        self.adjacency_table_mutable()
-            .entry(to.to_string())
-            .or_insert_with(Vec::new)
-            .push((from.to_string(), weight));
-    }
-    
+    fn add_node(&mut self, node: &str) -> bool;
+    fn add_edge(&mut self, edge: (&str, &str, i32));
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
     }
@@ -79,6 +62,8 @@ pub trait Graph {
         edges
     }
 }
+
+
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
